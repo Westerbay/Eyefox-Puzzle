9f54102edfe3994c94796e7261ceab65a6e44988
Wester <mathis.dubuisson@ecole.ensicaen.fr>
2025-07-19 21:00:35 +0200

    Update Responsive

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-var sprite = document.getElementById("sprite");
-var indexSprite = 0;
+var awake = document.getElementById("awake");
+var indexAwake = 0;
+
+var sleeping = document.getElementById("sleeping");
+var indexSleeping = 0;
+
+var stand = document.getElementById("stand");
+var indexStand = 0;
 
 function nextSprite() {
-    indexSprite = (indexSprite + 1) % 5;
-    sprite.src = `images/awake${indexSprite + 1}.png`;
+    indexAwake = (indexAwake + 1) % 5;
+    awake.src = `images/awake${indexAwake + 1}.png`;
+
+    indexSleeping = (indexSleeping + 1) % 6;
+    sleeping.src = `images/sleeping${indexSleeping + 1}.png`;
+
+    indexStand = (indexStand + 1) % 5;
+    stand.src = `images/stand${indexStand + 1}.png`;
 }
 
 setInterval(nextSprite, 160);
